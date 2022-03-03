d281460ee726fe614de5ef4585233a8b7273c6c0
Harshitha DN <97090084+Harshithadn@users.noreply.github.com>
2022-03-03 09:43:11 +0000

    p7

--- p7final.c
+++ p7final.c
@@ -15,7 +15,35 @@ int gcd(int a,int b)
     }
   return a;
 }
+Fraction input_Fraction()
+{
+  Fraction f;
+  printf("input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+Fraction add_fractions(Fraction f1,Fraction f2)
+{
+  Fraction sum;
+  sum.num=f1.num*f2.den+f2.num*f1.den;
+  sum.den=f1.den*f2.den;
+  int g=gcd(sum.num,sum.den);
+  sum.num=sum.num/g;
+  sum.den=sum.den/g;
+  return sum;
+}
+void output(Fraction f1, Fraction f2, Fraction sum)
+{
+  printf("the sum of fraction %d/%d and %d/%d is %d/%d\n",f1.num,
+  f1.den,f2.num,f2.den,sum.num,sum.den);
+}
+
 int main()
 {
-  printf("%d\n",gcd(16,24));
+  Fraction f1,f2,sum;
+  f1 =input_Fraction();
+  f2 =input_Fraction();
+  sum =add_fractions(f1,f2);
+  output(f1,f2,sum);
+  return 0;
 }
